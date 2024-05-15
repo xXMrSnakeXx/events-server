228e8605eecf86b816ab1221f5df450ed95b9fd0
Vladyslav Rozhenko <pochta.vlad@ukr.net>
2024-05-15 12:25:53 +0300

    add totalEvents

--- controllers/events/getEvents.js
+++ controllers/events/getEvents.js
@@ -3,6 +3,10 @@ import { Event } from "../../models/index.js";
 export const getEvents = async (req, res) => {
   const { page, limit } = req.query;
   const skip = (page - 1) * limit;
+  const totalEvents = await Event.countDocuments({});
   const result = await Event.find().skip(skip).limit(limit);
-  res.json(result);
+  res.json({
+    events: result,
+    totalEvents
+  });
 };
